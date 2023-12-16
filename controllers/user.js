31b20e198ebec03781b589c9860a01a72085b6c7
MinhLong <longbo1208@gmail.com>
2023-12-17 01:27:32 +0700

    CRUD User

--- controllers/user.js
+++ controllers/user.js
@@ -36,15 +36,15 @@ const login = asyncHandler(async (req, res) => {
     const response = await User.findOne({ email })
     if (response && await response.isCorrectPassword(password)) {
         // Remove pwd, role form response
-        const { password, role, ...userData } = response.toObject()
+        const { password, role, refreshToken, ...userData } = response.toObject()
         // Create access token after user login
         const accessToken = generateAccessToken(response._id, role)
         // Create refresh Token after user login
-        const refreshToken = generateRefreshToken(response._id)
+        const newRefreshToken = generateRefreshToken(response._id)
         // Save refresh Token in DB
-        await User.findByIdAndUpdate(response._id, { refreshToken }, { new: true })
+        await User.findByIdAndUpdate(response._id, { refreshToken: newRefreshToken }, { new: true })
         // Save refresh Token on cookie
-        res.cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 })// millisecond in 30 days
+        res.cookie('refreshToken', newRefreshToken, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 })// millisecond in 30 days
         return res.status(200).json({
             success: true,
             accessToken,
@@ -59,7 +59,7 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     const { _id } = req.user
     const user = await User.findById(_id).select('-refreshToken -role -password')
     return res.status(200).json({
-        success: false,
+        success: user ? true : false,
         rs: user ? user : "User not found !!!"
     })
 })
@@ -136,6 +136,45 @@ const resetPwd = asyncHandler(async (req, res) => {
         mes: user ? 'Updated password' : 'Something went wrong'
     })
 })
+
+const getUsers = asyncHandler(async (req, res) => {
+    const response = await User.find().select('-refreshToken -role -password')
+    return res.status(200).json({
+        success: response ? true : false,
+        user: response
+    })
+})
+
+const deleteUser = asyncHandler(async (req, res) => {
+    const { _id } = req.query;
+    if (!_id) throw new Error('Missing inputs')
+    const response = await User.findByIdAndDelete(_id)
+    return res.status(200).json({
+        success: response ? true : false,
+        deletedUser: response ? `User with email: ${response.email} deleted` : "No user delete"
+    })
+})
+
+const updateUser = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    if (!_id || Object.keys(req.body).length === 0) throw new Error('Missing inputs')
+    const response = await User.findByIdAndUpdate(_id, req.body, { new: true }).select('-password -role -refreshToken')
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedUser: response ? response : "Some thing went wrongs !!!"
+    })
+})
+
+const updateUserByAdmin = asyncHandler(async (req, res) => {
+    const { uid } = req.params;
+    if (Object.keys(req.body).length === 0) throw new Error('Missing inputs')
+    const response = await User.findByIdAndUpdate(uid, req.body, { new: true }).select('-password -role -refreshToken')
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedUser: response ? response : "Some thing went wrongs !!!"
+    })
+})
+
 module.exports = {
     register,
     login,
@@ -144,4 +183,8 @@ module.exports = {
     logout,
     forgotPassword,
     resetPwd,
+    getUsers,
+    deleteUser,
+    updateUser,
+    updateUserByAdmin
 }
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,20 @@
 const router = require('express').Router()
 const ctrls = require('../controllers/user')
-const { verifyAccessToken } = require('../middlewares/verifyToken')
+const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
 
 router.post('/register', ctrls.register);
 router.post('/login', ctrls.login);
-router.get('/current', verifyAccessToken, ctrls.getCurrentUser);
 router.post('/refreshToken', ctrls.refreshAccessToken);
 router.get('/logout', ctrls.logout);
 router.get('/forgotPassword', ctrls.forgotPassword);
 router.put('/resetPassword', ctrls.resetPwd);
+
+//Route For Admin
+router.get('/', [verifyAccessToken, isAdmin], ctrls.getUsers)
+router.put('/:uid', [verifyAccessToken, isAdmin], ctrls.updateUserByAdmin)
+router.delete('/', [verifyAccessToken, isAdmin], ctrls.deleteUser)
+
+router.get('/current', verifyAccessToken, ctrls.getCurrentUser);
+router.put('/current', [verifyAccessToken], ctrls.updateUser)
 
 module.exports = router;
